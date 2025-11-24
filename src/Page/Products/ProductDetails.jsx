9eb10ec08b0fed33b5de7bcb7928b59a887b11a4
sujanchakma1 <sujanckz926@gmail.com>
2025-11-25 00:56:12 +0600

    added product details error handle

--- src/Page/Products/ProductDetails.jsx
+++ src/Page/Products/ProductDetails.jsx
@@ -2,92 +2,80 @@ import UseAxiosSecure from "@/Hook/UseAxiosSecure";
 import Loading from "@/Shared/Loading";
 import { useQuery } from "@tanstack/react-query";
 import { Link, useParams } from "react-router";
-import { useState } from "react";
 import { toast } from "react-toastify";
 import UseAuth from "@/Hook/UseAuth";
 
 const ProductDetails = () => {
   const { id } = useParams();
   const axiosSecure = UseAxiosSecure();
-  const {user} = UseAuth()
+  const { user } = UseAuth();
 
-  // Quantity state
-  const [qty, setQty] = useState(1);
 
   // Fetch ALL products (your backend structure)
-  const { data, isLoading } = useQuery({
-    queryKey: ["products"],
+  const { data: products, isLoading } = useQuery({
+    queryKey: ["product"],
     queryFn: async () => {
-      const res = await axiosSecure.get("/products");
-      return res.data.products; // array
+      const res = await axiosSecure.get("/product");
+      return res.data; // array
     },
   });
 
   if (isLoading) return <Loading />;
 
   // find this product
-  const product = data.find((p) => p._id === id);
+  const product = products.find((p) => p._id === id);
 
   if (!product) {
     return <div className="text-center py-20 text-xl">Product not found</div>;
   }
 
   // Recommended (same category, excluding itself)
-  const recommended = data
+  const recommended = products
     .filter((p) => p.category === product.category && p._id !== id)
     .slice(0, 4);
-
-  // Quantity handlers
-  const increaseQty = () => {
-    if (qty < product.stock) setQty(qty + 1);
-  };
-
-  const decreaseQty = () => {
-    if (qty > 1) setQty(qty - 1);
-  };
 
 
   // * Added to Cart
 
   const handleAddToCart = async (product) => {
-      const {
-        _id,
-        name,
-        description,
-        brand,
-        category,
-        image,
-        price,
-        discountPrice,
-        stock,
-      } = product;
-  
-      const productData = {
-        productId: _id,
-        productName: name,
-        description,
-        brand,
-        category,
-        image,
-        price,
-        discountPrice,
-        stock,
-        userName: user.displayName,
-        userEmail: user.email,
-      };
-  
-      try {
-        const res = await axiosSecure.post("/cartProduct", productData);
-        console.log("Cart Response:", res.data);
-        if(res.data.insertedId){
-          toast.success("Added to Cart")
-        } else {
+    const {
+      _id,
+      name,
+      description,
+      brand,
+      category,
+      image,
+      price,
+      discountPrice,
+      stock,
+    } = product;
+
+    const productData = {
+      productId: _id,
+      productName: name,
+      description,
+      brand,
+      category,
+      image,
+      price,
+      discountPrice,
+      stock,
+      userName: user.displayName,
+      userEmail: user.email,
+    };
+
+    try {
+      const res = await axiosSecure.post("/cartProduct", productData);
+      console.log("Cart Response:", res.data);
+      if (res.data.insertedId) {
+        toast.success("Added to Cart");
+      } else {
         toast.error("Failed to add!");
       }
-      } catch (error) {
-        toast.error("Add to Cart Error:",error);
-      }
-    };
+    } catch (error) {
+      toast.error("Add to Cart Error:", error);
+    }
+  };
 
   return (
     <div className="container max-w-7xl mx-auto p-6">
@@ -125,37 +113,18 @@ const ProductDetails = () => {
                 ${product.price}
               </span>
             )}
-          </div>
-
-          <p className="text-sm mb-2">
-            Available Stock: {product.stock}
-          </p>
-
-          {/* QUANTITY SELECTOR */}
-          <div className="flex items-center gap-4 mb-6">
-            <button
-              onClick={decreaseQty}
-              className="px-3 py-1 bg-gray-500 rounded-md text-xl"
-            >
-              -
-            </button>
-
-            <span className="text-xl font-semibold">{qty}</span>
-
-            <button
-              onClick={increaseQty}
-              className="px-3 py-1 bg-gray-500 rounded-md text-xl"
-            >
-              +
-            </button>
+            <p className="text-md font-medium mb-2">Available Stock: {product.stock}</p>
           </div>
 
           {/* ACTION BUTTONS */}
           <div className="flex gap-4">
-            <button onClick={()=>handleAddToCart(product)} className="btn btn-secondary text-white rounded-md">
+            <button
+              onClick={() => handleAddToCart(product)}
+              className="btn btn-secondary btn-outline rounded-md"
+            >
               Add to Cart
             </button>
-            <Link to={`/product/buy/${product._id}`}>
+            <Link to={`/products/buy/${product._id}`}>
               <button className="btn btn-primary text-white rounded-md ">
                 Buy Now
               </button>
@@ -170,9 +139,7 @@ const ProductDetails = () => {
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
         {recommended.map((item) => (
           <Link key={item._id} to={`/products/details/${item._id}`}>
-            <div
-              className="bg-white p-4 rounded-lg shadow hover:shadow-md transition"
-            >
+            <div className="bg-white p-4 rounded-lg shadow hover:shadow-md transition">
               <img
                 src={item.image}
                 alt={item.name}
