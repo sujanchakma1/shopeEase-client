e27446ebc5087070a5fb21305891fa024244032b
sujanchakma1 <sujanckz926@gmail.com>
2025-12-09 20:54:32 +0600

    added pay & my order

--- src/Page/Dashboard/User/Payments.jsx
+++ src/Page/Dashboard/User/Payments.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import { loadStripe } from "@stripe/stripe-js";
+import React from "react";
+import PaymentForm from "./PaymentForm";
+import { Elements } from "@stripe/react-stripe-js";
 
+const stripePromise = loadStripe(import.meta.env.VITE_public_key);
 const Payments = () => {
   return (
     <div>
-      
+      <h2 className="font-bold text-3xl text-center py-5">Please Pay</h2>
+      <Elements stripe={stripePromise}>
+        <PaymentForm></PaymentForm>
+      </Elements>
     </div>
   );
 };
