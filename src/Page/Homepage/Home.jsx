bd97f4fa2f8b0d4c6cbf2d629e8d008779a01488
sujanchakma1 <sujanckz926@gmail.com>
2025-12-12 23:14:47 +0600

    added populer products

--- src/Page/Homepage/Home.jsx
+++ src/Page/Homepage/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import HeroSection from './HeroSection';
 import { Partners } from './Partners';
 import FeatureCategories from './FeatureCategories';
+import PopularProducts from './PopularProducts';
 
 const Home = () => {
   return (
     <div>
       <HeroSection></HeroSection>
       <FeatureCategories></FeatureCategories>
+      <PopularProducts></PopularProducts>
       <Partners></Partners>
     </div>
   );
